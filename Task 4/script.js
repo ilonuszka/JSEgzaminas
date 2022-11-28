8e0c62d36d4f145d0b4919520f1ed19735b7f030
4porthub <109093472+ilonuszka@users.noreply.github.com>
2022-11-28 21:05:59 +0200

    task 4 - ddone

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,3 +9,73 @@ būti stilizuota su CSS ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'cars.json';
+const output = document.querySelector("#output");
+
+    fetch(ENDPOINT)
+        .then((response) => response.json())
+        .then((data) => printResults(data));
+    output.textContent = '';
+
+const printResults = data => {
+    data.forEach(item=>{
+        
+        Object.assign(output.style,{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center"
+        })
+
+        const card = document.createElement('div');
+        Object.assign(card.style, {
+            backgroundColor: "	#dcdcdc",
+            borderRadius: "5px",
+            margin:"5px",
+            width: "200px",
+            boxShadow: "0px 0px 8px 1px #000000",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center"
+        })
+        
+        const brand = document.createElement('p');
+        Object.assign(brand.style, {
+            borderRadius: "5px",
+            margin:"5px",
+            fontSize: '1em',
+            fontWeight: '800'
+        })
+        brand.textContent = item.brand;
+        console.log(item.brand);
+        card.append(brand);
+
+        const models = document.createElement("div");
+        Object.assign(models.style, {
+            borderRadius: "3px",
+            margin:"5px",
+            width: "100%",
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            alignItems: "center"
+        })
+
+        item.models.forEach(model=>{
+            const modelSpan = document.createElement("span");
+            modelSpan.textContent = model;
+            Object.assign(modelSpan.style, {
+                borderRadius: "5px",
+                margin:"0px 5px 0px 5px",
+                padding: "2px",
+                fontSize: '0.5em',
+                fontWeight: '400',
+                margin: "1px",
+                border: "black solid 1px"
+            })
+            models.append(modelSpan);
+        })
+        card.append(models);
+
+        output.append(card);
+    })
+}
