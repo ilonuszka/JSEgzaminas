a6191df8665a465176cf9adddc775f475414c19d
4porthub <109093472+ilonuszka@users.noreply.github.com>
2022-11-28 22:18:07 +0200

    Task 8 - done
    All tasks done

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,26 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(a, b) {
+    this.a = a;
+    this.b = b;
+    this.sum = function () {
+        return a + b;
+    };
+    this.subtraction = function () {
+        return a - b;
+    };
+    this.multiplication = function () {
+        return a * b;
+    };
+    this.division = function () {
+        return a / b;
+    };
+}
+
+const create = new Calculator(80, 30);
+console.log(`This sum is ${create.sum()}`);
+console.log(`This substraction is ${create.subtraction()}`);
+console.log(`This multiplication is ${create.multiplication()}`);
+console.log(`This division is ${create.division().toFixed(2)}`);  
