c515c99232f46b2c5eaa43272f60602dff86b5cc
4porthub <109093472+ilonuszka@users.noreply.github.com>
2022-11-28 19:02:52 +0200

    1 task - done

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,42 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: rezultatas turi būti matomas pateikus formą ir atvaizduojamas
 <div id="output"></div> viduje. Gautus atsakymus stilizuokite naudojant CSS;
 ------------------------------------------------------------------- */
+
+const form = document.querySelector("form");
+const output = document.querySelector("#output");
+
+form.addEventListener('submit', evt=>{
+    evt.preventDefault();
+    const kg= Number(evt.target.search.value);
+    const lb = kg * 2.2046;
+    const grams = kg / 0.0010000;
+    const oz = kg * 35.274;
+
+    Object.assign(output.style, {
+        maxWidth: "80%",
+        margin: "50px auto",
+        fontSize: '1.2em',
+        fontWeight: '800'
+    })
+
+    output.textContent = '';
+
+    const textOutput = document.createElement('p');
+    textOutput.textContent = `${String(kg)} kg after conversion will be:`;
+    output.append(textOutput);
+
+    const lbOutput = document.createElement('p');
+    lbOutput.style.color = 'red';
+    lbOutput.textContent = String(lb) + ' lb';
+    output.append(lbOutput);
+    
+    const gOutput = document.createElement('p');
+    gOutput.style.color = 'blue';
+    gOutput.textContent = String(grams) + ' g';
+    output.append(gOutput);
+    
+    const ozOutput = document.createElement('p');
+    ozOutput.style.color = 'green';
+    ozOutput.textContent = String(oz) + '  oz';
+    output.append(ozOutput);
+})
