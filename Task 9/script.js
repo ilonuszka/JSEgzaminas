40878db25dbc1c22d19be8907a542286a02826c8
4porthub <109093472+ilonuszka@users.noreply.github.com>
2022-11-28 21:41:23 +0200

    task 9 - done

--- Task 9/script.js
+++ Task 9/script.js
@@ -9,3 +9,24 @@ budget: number
 Metodas: 
 wasExpensive() - jeigu filmo "budget" yra daugiau nei 100 000 000 mln USD, tada grąžins true, kitu atveju false. 
 ------------------------------------------------------------------------------------------------------ */
+
+class Movie {
+
+    constructor(title, director, budget) {
+        this.title = title;
+        this.director = director;
+        this.budget = budget;
+
+    };
+
+    wasExpensive = function () {
+        if (this.budget > 100000000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+const movieMaker = new Movie('A Walk to Remember', 'A.Strong', 120000);
+console.log(movieMaker.wasExpensive(), '--> ' + movieMaker.budget + ' USD');
